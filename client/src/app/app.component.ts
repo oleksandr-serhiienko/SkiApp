8bd14ca3407d697af84dab1ce253496f738b6acb
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-06-21 20:44:16 +0200

    check

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { createWriteStream } from 'fs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+
+  constructor(private http: HttpClient) { }
+    
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
+      console.log(response);
+    }, error => { console.log(error) });
+    }
 }
