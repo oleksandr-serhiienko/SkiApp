98ea90155e9ead494aa4cec398a765df0f8611db
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-06-26 11:27:57 +0200

    finished adding typesripts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { createWriteStream } from 'fs';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,12 @@ import { createWriteStream } from 'fs';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) { }
     
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
       console.log(response);
       this.products = response.date;
     }, error => {
