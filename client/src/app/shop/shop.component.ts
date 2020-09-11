5c73f164044db96d83f63b498eaf2978844d36ce
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-09-11 10:59:12 +0200

    added filters and more data, so now we have the base for the products

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.css']
+})
+export class ShopComponent implements OnInit {
+  products: IProduct[];
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit(): void {
+    this.shopService.getProducts().subscribe(response => {
+      this.products = response.date
+    }, error => { console.log(error); });
+  }
+
+}
